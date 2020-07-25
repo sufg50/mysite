baf1ad1acf6dcb13439504ede30d56f9d73cea24
Ito <ahceru2@yahoo.co.jp>
2020-07-26 01:12:42 +0900

    Add Procfile + varchar = 191

--- task_portfolio/app/Providers/AppServiceProvider.php
+++ task_portfolio/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // herokuのために追記
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //herokuのために追記
+        Schema::defaultStringLength(191);
     }
 }
